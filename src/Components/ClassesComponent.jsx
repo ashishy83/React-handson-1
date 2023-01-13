4068977b1638c869bb0bfaaf531daa2a280bdc0c
Ashish <enigmaticwolf83@gmail.com>
2023-01-13 20:48:12 +0530

    added ClassesComponent

--- src/Components/ClassesComponent.jsx
+++ src/Components/ClassesComponent.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
+import React from "react";
+import { Component } from "react";
 
-const ClassesComponent = () => {
-  return (
-    <div className='components c'>
-         <div className='comps'>
-            <h1 style={{textAlign: 'center'}}>This is Created using Functional Component</h1>
-            <p className='p2'>This is done using External CSS</p>
-            <p style={{textAlign: 'center', color: 'red'}}>THis is done using Inline CSS</p>
+// const ClassesComponent = () => {
+//   return (
+//     <div className='components c'>
+//          <div className='comps'>
+//             <h1 style={{textAlign: 'center'}}>This is Created using Functional Component</h1>
+//             <p className='p2'>This is done using External CSS</p>
+//             <p style={{textAlign: 'center', color: 'red'}}>THis is done using Inline CSS</p>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default ClassesComponent
+
+class ClassesComponent extends Component {
+  render() {
+    return (
+      <div className="components c">
+        <div className="comps">
+          <h1 style={{ textAlign: "center" }}>
+            This is Created using Functional Component
+          </h1>
+          <p className="p2">This is done using External CSS</p>
+          <p style={{ textAlign: "center", color: "red" }}>
+            THis is done using Inline CSS
+          </p>
         </div>
-    </div>
-  )
+      </div>
+    );
+  }
 }
+export default ClassesComponent;
 
-export default ClassesComponent
 
